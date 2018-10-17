1ebca670e9c4a733e09fd87fffc53c63c304ca3b
lewdev <lewdev@gmail.com>
2018-10-16 22:09:10 -1000

    Added new improved fue sounds.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var displaySongDiv = document.getElementById("displaySong");
 var soundNameList = [
   "Signal", "Hyoshigi", "Chanpon", "Kotsuzumi", "Taiko",
   "Surigane-1", "Surigane-2",
-  "Fue-2", "Fue-3", "Fue-5", "Fue-6", "Fue-6-high", "Fue-7",
+  "Fue-2", "Fue-3", "Fue-5", "Fue-5-high", "Fue-6", "Fue-6-high", "Fue-7",
   "Koto-13", "Koto-12", "Koto-11", "Koto-10", "Koto-9",
   "Koto-8", "Koto-7", "Koto-6", "Koto-5", "Koto-4", "Koto-3",
   "Koto-2", "Koto-1",
